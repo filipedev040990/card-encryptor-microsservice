4b3a37f0f2a00fabc2a4300014fc1f6596c3327a
Filipe Siqueira <filipedev040990@gmail.com>
2023-07-10 16:43:19 -0300

    feat: add route authenticate

--- src/infra/routes.ts
+++ src/infra/routes.ts
@@ -3,9 +3,11 @@ import { expressRouteAdapter } from './adapters/express-route.adapter'
 import { makeSaveCardController } from './factories/controllers/save-card.factory'
 import { makeGetCardByIdController } from './factories/controllers/get-card-by-id.factory'
 import { makeDeleteCardController } from './factories/controllers/delete-card.factory'
+import { makeAuthenticateController } from './factories/controllers/authenticate.factory'
 
 const router = Router()
 
+router.post('/auth', expressRouteAdapter(makeAuthenticateController()))
 router.post('/card', expressRouteAdapter(makeSaveCardController()))
 router.get('/card/:id', expressRouteAdapter(makeGetCardByIdController()))
 router.delete('/card/:id', expressRouteAdapter(makeDeleteCardController()))
