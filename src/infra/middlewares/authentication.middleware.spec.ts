92e5b1a11bb71a6b12270f4263c0331e77194d2c
Filipe Siqueira <filipedev040990@gmail.com>
2023-07-11 11:45:28 -0300

    ensure AuthenticationMiddleware call Token.validate once and woth correct token

--- src/infra/middlewares/authentication.middleware.spec.ts
+++ src/infra/middlewares/authentication.middleware.spec.ts
@@ -1,18 +1,24 @@
 import { InputController } from '@/shared/types'
 import { AuthenticationMiddleware } from './authentication.middleware'
+import { mock } from 'jest-mock-extended'
+import { TokenInterface } from '@/application/interfaces/token.interface'
 
 describe('AuthenticationMiddleware', () => {
   let sut: AuthenticationMiddleware
   let input: InputController
 
+  const token = mock<TokenInterface>()
+
   beforeEach(() => {
-    sut = new AuthenticationMiddleware()
+    sut = new AuthenticationMiddleware(token)
 
     input = {
       headers: {
         authorization: 'Bearer XPTO'
       }
     }
+
+    token.validate.mockResolvedValue('any Response')
   })
 
   test('should return 403 if Auhtorization header is not provided', async () => {
@@ -29,5 +35,12 @@ describe('AuthenticationMiddleware', () => {
     const output = await sut.execute(input)
 
     expect(output).toEqual({ statusCode: 403, body: 'Forbidden: JWT is required' })
+  })
+
+  test('should call Token.validate once and with correct token', async () => {
+    await sut.execute(input)
+
+    expect(token.validate).toHaveBeenCalledTimes(1)
+    expect(token.validate).toHaveBeenCalledWith('XPTO')
   })
 })
--- src/infra/middlewares/authentication.middleware.ts
+++ src/infra/middlewares/authentication.middleware.ts
@@ -1,10 +1,16 @@
+import { TokenInterface } from '@/application/interfaces/token.interface'
 import { forbiddenError } from '@/shared/helpers/http.helper'
 import { InputController } from '@/shared/types'
 
 export class AuthenticationMiddleware {
+  constructor (private readonly token: TokenInterface) {}
+
   async execute (input: InputController): Promise<any> {
     if (!input?.headers?.authorization) {
       return forbiddenError()
     }
+
+    const token = input.headers.authorization.split('Bearer ')[1]
+    await this.token.validate(token)
   }
 }
