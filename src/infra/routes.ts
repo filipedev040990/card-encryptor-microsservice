fa14366cb6d9711ab80207ed8276fa5b0dc3eda2
Filipe Siqueira <filipedev040990@gmail.com>
2023-07-11 12:12:55 -0300

    feat: add middleware in routes

--- src/infra/routes.ts
+++ src/infra/routes.ts
@@ -4,12 +4,15 @@ import { makeSaveCardController } from './factories/controllers/save-card.factor
 import { makeGetCardByIdController } from './factories/controllers/get-card-by-id.factory'
 import { makeDeleteCardController } from './factories/controllers/delete-card.factory'
 import { makeAuthenticateController } from './factories/controllers/authenticate.factory'
+import { makeAuthenticationMiddleware } from './factories/middlewares/authentication.factory'
+import { expressMiddlewareAdapter } from './adapters/express-middleware.adapter'
 
 const router = Router()
 
 router.post('/auth', expressRouteAdapter(makeAuthenticateController()))
-router.post('/card', expressRouteAdapter(makeSaveCardController()))
-router.get('/card/:id', expressRouteAdapter(makeGetCardByIdController()))
-router.delete('/card/:id', expressRouteAdapter(makeDeleteCardController()))
+
+router.post('/card', expressMiddlewareAdapter(makeAuthenticationMiddleware()), expressRouteAdapter(makeSaveCardController()))
+router.get('/card/:id', expressMiddlewareAdapter(makeAuthenticationMiddleware()), expressRouteAdapter(makeGetCardByIdController()))
+router.delete('/card/:id', expressMiddlewareAdapter(makeAuthenticationMiddleware()), expressRouteAdapter(makeDeleteCardController()))
 
 export { router }
